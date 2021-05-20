3fbdc6d43cdb4802fa7c0981bed189977ea603b9
marco <g.marcotaba@gmail.com>
2021-05-20 10:28:07 +0200

    added event component

---
+++ src/Events.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export const Events = () => {
+    return (
+        <>
+            <div className='events-container'>
+                Eventi dal campo
+            </div>
+        </>
+    )
+}
